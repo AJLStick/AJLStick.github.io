11f56d7a5df370f55559c82fa1e98c1f70c6b4b6
AJLStick <aailoc6693@student.ccs.k12.nc.us>
2020-12-03 08:16:26 -0500

    Delete senddata.php

--- senddata.php
+++
@@ -1,9 +0,0 @@
-php
- $path = 'data.txt';
- if (isset($_POST['field1']) && isset($_POST['field2'])) {
-    $fh = fopen($path,"a+");
-    $string = $_POST['field1'].' - '.$_POST['field2'];
-    fwrite($fh,$string); // Write information to the file
-    fclose($fh); // Close the file
- }
-?>
